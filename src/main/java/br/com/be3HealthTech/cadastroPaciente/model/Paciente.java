f3cf6c34f07e84be86ebc68b2e6055b21a4b38b4
Raqueldcsilva <raquelshdy@gmail.com>
2021-12-18 10:16:37 -0300

    inseri os campos de prontuario e o de celular

--- src/main/java/br/com/be3HealthTech/cadastroPaciente/model/Paciente.java
+++ src/main/java/br/com/be3HealthTech/cadastroPaciente/model/Paciente.java
@@ -24,6 +24,9 @@ public class Paciente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	@NotNull(message = "Aviso! o número do prontuário é obrigatório")
+	private long numeroProntuario;
+	
 	@Column(unique=true)//único
 	private String cpf;
 
@@ -49,6 +52,9 @@ public class Paciente {
 	
 	private long telefone;
 	
+	private long celular;
+	
+	
 	@ManyToOne
 	@JsonIgnoreProperties("convenio")
 	private Convenio convenio;
@@ -59,82 +65,121 @@ public class Paciente {
 		return id;
 	}
 
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
+
+	public long getNumeroProntuario() {
+		return numeroProntuario;
+	}
+
+
+	public void setNumeroProntuario(long numeroProntuario) {
+		this.numeroProntuario = numeroProntuario;
+	}
+
 
 	public String getCpf() {
 		return cpf;
 	}
 
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 
 	public String getNome() {
 		return nome;
 	}
 
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 
 	public String getSobrenome() {
 		return sobrenome;
 	}
 
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
+
 
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
 
+
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
+
 
 	public String getGenero() {
 		return genero;
 	}
 
+
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
+
 
 	public String getRg() {
 		return rg;
 	}
 
+
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
+
 
 	public String getEmail() {
 		return email;
 	}
 
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 
 	public long getTelefone() {
 		return telefone;
 	}
 
+
 	public void setTelefone(long telefone) {
 		this.telefone = telefone;
 	}
+
+
+	public long getCelular() {
+		return celular;
+	}
+
+
+	public void setCelular(long celular) {
+		this.celular = celular;
+	}
+
 
 	public Convenio getConvenio() {
 		return convenio;
 	}
 
+
 	public void setConvenio(Convenio convenio) {
 		this.convenio = convenio;
 	}
 
-	
+
 			
 }
